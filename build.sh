ccdef1dc9b16ceb277095460b95042ced58e4e88
Ashok Kumar R Asaithambi <iamraashokkumar@gmail.com>
2022-04-11 18:31:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 version=`grep -i version properties | cut -f2 -d '='`
 echo $version
-
+echo "Build the Java application"
 docker build -t iamraashoka/java-app:$version .
+echo "Push the $version to docker hub"
 docker push iamraashoka/java-app:$version
+echo "Run the docker with $version"
+docker run -dit --name java-app iamraashoka/java-app:$version
